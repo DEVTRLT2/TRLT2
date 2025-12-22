7f5ef427c51ef8ed726e4e4d63b042448abb2f94
DEVTRLT2 <ggokkokoko@gmail.com>
2025-12-22 17:12:39 +0700

    Create nampak tidak berguna tapi berguna banget.lua

---
+++ nampak tidak berguna tapi berguna banget.lua
@@ -0,0 +1,151 @@
+return {
+
+    ["AxeClass_AxeSwamp"] = {
+        Damage = 0.8,
+        SwingCooldown = 0.55,
+
+        SpecialTrees = {
+            ["SwampTree"] = {
+                Damage = 1.2,
+                SwingCooldown = 0.45,
+            }
+        }
+    },
+
+    ["AxeClass_CandyCaneAxe"] = {
+        Damage = 0.0,
+        SwingCooldown = 0.7,
+
+        SpecialTrees = {
+            ["CandyTree"] = {
+                Damage = 2.5,
+                SwingCooldown = 0.6,
+            }
+        }
+    },
+
+    ["AxeClass_Beesaxe"] = {
+        Damage = 1.4,
+        SwingCooldown = 0.5,
+
+        SpecialTrees = {
+            ["BeeTree"] = {
+                Damage = 2.0,
+                SwingCooldown = 0.4,
+            }
+        }
+    },
+
+    ["AxeClass_AxeAmber"] = {
+        Damage = 3.39,
+        SwingCooldown = 1.0,
+
+        SpecialTrees = {
+            ["AmberTree"] = {
+                Damage = 6.0,
+                SwingCooldown = 0.9,
+            }
+        }
+    },
+
+    ["AxeClass_GingerbreadAxe"] = {
+        Damage = 1.2,
+        SwingCooldown = 0.5,
+
+        SpecialTrees = {
+            ["GingerbreadTree"] = {
+                Damage = 2.2,
+                SwingCooldown = 0.4,
+            }
+        }
+    },
+
+    ["AxeClass_ManyAxe"] = {
+        Damage = 10.2,
+        SwingCooldown = 1.9,
+
+        SpecialTrees = {
+            ["ManyTree"] = {
+                Damage = 15.0,
+                SwingCooldown = 1.5,
+            },
+            ["GoldTree"] = {
+                Damage = 20.0,
+                SwingCooldown = 1.2,
+            }
+        }
+    },
+
+    ["AxeClass_CaveAxe"] = {
+        Damage = 0.4,
+        SwingCooldown = 0.4,
+
+        SpecialTrees = {
+            ["CaveTree"] = {
+                Damage = 0.9,
+                SwingCooldown = 0.35,
+            }
+        }
+    },
+
+    ["AxeClass_CandyCornAxe"] = {
+        Damage = 1.75,
+        SwingCooldown = 0.6,
+
+        SpecialTrees = {
+            ["CandyCornTree"] = {
+                Damage = 3.0,
+                SwingCooldown = 0.5,
+            }
+        }
+    },
+
+    ["AxeClass_EndTimesAxe"] = {
+        Damage = 1.58,
+        SwingCooldown = 0.4,
+
+        SpecialTrees = {
+            ["EndTree"] = {
+                Damage = 2.5,
+                SwingCooldown = 0.3,
+            }
+        }
+    },
+
+    ["AxeClass_BluesteelAxe"] = {
+        Damage = 2.8,
+        SwingCooldown = 0.8,
+
+        SpecialTrees = {
+            ["BlueSteelTree"] = {
+                Damage = 4.5,
+                SwingCooldown = 0.65,
+            }
+        }
+    },
+
+    ["AxeClass_MintAxe"] = {
+        Damage = 0.8,
+        SwingCooldown = 5.0,
+
+        SpecialTrees = {
+            ["MintTree"] = {
+                Damage = 2.0,
+                SwingCooldown = 3.5,
+            }
+        }
+    },
+
+    ["AxeClass_IceAxe"] = {
+        Damage = 0.36,
+        SwingCooldown = 0.4,
+
+        SpecialTrees = {
+            ["IceTree"] = {
+                Damage = 0.9,
+                SwingCooldown = 0.3,
+            }
+        }
+    },
+
+}
